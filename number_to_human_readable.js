a94b78d2bb3d2f266c227b639cfa55e187d5e31a
duecorda <duecorda@gmail.com>
2020-10-16 22:52:09 -0700

    add js

---
+++ number_to_human_readable.js
@@ -0,0 +1,25 @@
+function number_to_human_readable(price) {
+  let digits = String(price).split('').reverse().join('');
+  if(digits.length <= 0) return '0원';
+
+  const price_units = ['','만','억','조','경'];
+  const read_units = ['','십','백','천'];
+  const number_units = ['','일','이','삼','사','오','육','칠','팔','구'];
+
+  let ret = [];
+  let group = digits.match(/.{1,4}/g);
+  for(let i=0;i<group.length;i++) {
+    let subret = [];
+    let elements = group[i]; 
+    let pu = price_units[i];
+    for(let j=0;j<elements.length;j++) {
+      if(elements[j] == "0") continue;
+      let ru = read_units[j];
+      let nu = number_units[elements[j]];
+      if(ru !== '' && nu == '일') nu = '';
+      subret.push(nu+ru);
+    }
+    ret.push(subret.reverse().join('')+pu);
+  }
+  return ret.reverse().join(' ')+'원';
+}
