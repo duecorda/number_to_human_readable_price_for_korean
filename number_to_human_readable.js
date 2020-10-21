1adfe1d9ea0ef0d8df5f582766852e2676fc637a
duecorda <duecorda@gmail.com>
2020-10-20 19:36:56 -0700

    테스트 HTML 추가, 불필요한 단위 표기 오류 수정

--- number_to_human_readable.js
+++ number_to_human_readable.js
@@ -2,7 +2,7 @@ function number_to_human_readable(price) {
   let digits = String(price).split('').reverse().join('');
   if(digits.length <= 0) return '0원';
 
-  const price_units = ['','만','억','조','경'];
+  const price_units = ['','만','억','조','경','해','자','양','구','간','정','재','극','항하사','아승기','나유타','불가사의','무량대수'];
   const read_units = ['','십','백','천'];
   const number_units = ['','일','이','삼','사','오','육','칠','팔','구'];
 
@@ -19,6 +19,7 @@ function number_to_human_readable(price) {
       if(ru !== '' && nu == '일') nu = '';
       subret.push(nu+ru);
     }
+    if(subret.length <= 0) continue;
     ret.push(subret.reverse().join('')+pu);
   }
   return ret.reverse().join(' ')+'원';
